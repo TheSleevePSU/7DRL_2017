74dd286dfb7396c93004ecfd79620a7cda15857c
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-03-04 14:41:41 -0800

    Added PlayerController with basic movement and GameManager with basic singleton code

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+ 
+/// GameManager is a singleton.
+/// To avoid having to manually link an instance to every class that needs it, it has a static property called
+/// instance, so other objects that need to access it can just call:
+///        GameManager.instance.DoSomeThing();
+///
+public class GameManager : MonoBehaviour {
+   // s_Instance is used to cache the instance found in the scene so we don't have to look it up every time.
+   private static GameManager s_Instance = null;
+   public enum GameState { Paused, Running }
+
+   // This defines a static instance property that attempts to find the manager object in the scene and
+   // returns it to the caller.
+   public static GameManager instance {
+       get {
+           if (s_Instance == null) {
+               // This is where the magic happens.
+               //  FindObjectOfType(...) returns the first GameManager object in the scene.
+               s_Instance =  FindObjectOfType(typeof (GameManager)) as GameManager;
+           }
+
+           // If it is still null, create a new instance
+           if (s_Instance == null) {
+               GameObject obj = new GameObject("GameManager");
+               s_Instance = obj.AddComponent(typeof (GameManager)) as GameManager;
+               Debug.Log("Could not locate an GameManager object. GameManager was Generated Automatically.");
+           }
+
+           return s_Instance;
+       }
+   }
+
+   // Ensure that the instance is destroyed when the game is stopped in the editor.
+   void OnApplicationQuit() {
+       s_Instance = null;
+   }
+
+   // Add the rest of the code here...
+   public void DoSomeThing() {
+       Debug.Log("Doing something now", this);
+   }
+
+   void Start()
+   {
+        
+   }
+
+   void Update()
+   {
+       if (Input.GetKeyDown(KeyCode.Space)) {
+           Debug.Log("Space key was pressed");
+       }
+   }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector3 targetPosition;
+	public Vector3 startPosition;
+	private float startTime;
+	private float journeyLength;
+
+	public float speed = 1.0f;
+	public float minDistance = 0.05f;
+	
+	// Use this for initialization
+	void Start () {
+		targetPosition = this.transform.position;
+		startPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UpdateControl();
+		UpdateMovement();
+	}
+
+	void UpdateControl()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow)
+		||  Input.GetKeyDown(KeyCode.DownArrow)
+		||  Input.GetKeyDown(KeyCode.LeftArrow)
+		||  Input.GetKeyDown(KeyCode.RightArrow)
+		   )
+		{
+			startPosition = transform.position;
+			startTime = Time.time;
+			
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				targetPosition = transform.position + new Vector3(0, 1, 0);
+			}
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				targetPosition = transform.position + new Vector3(0, -1, 0);
+			}
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				targetPosition = transform.position + new Vector3(-1, 0, 0);
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				targetPosition = transform.position + new Vector3(1, 0, 0);
+			}
+
+			journeyLength = Vector3.Distance(targetPosition, startPosition);
+		}
+	}
+
+	void UpdateMovement()
+	{
+		if (Vector3.Distance(startPosition, targetPosition) <= minDistance)
+		{
+			transform.position = targetPosition;
+			startPosition = targetPosition;
+		}
+		else
+		{
+			float distCovered = (Time.time - startTime) * speed;
+			float fracJourney = distCovered / journeyLength;
+			transform.position = Vector3.Lerp(startPosition, targetPosition, fracJourney);
+		}
+	}
+}
