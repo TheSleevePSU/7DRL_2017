f932e1a270fbbae1188deb70a284147cab489ea3
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-03-06 00:11:28 -0800

    Added Utility methods Shuffle and IsWalkable

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Utilities
+{
+    private static System.Random rng = new System.Random();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    public static bool IsWalkable(Vector2 position)
+    {
+        bool isWalkable = true;
+        Collider2D[] c2d = Physics2D.OverlapPointAll(position);
+        foreach (Collider2D c in c2d)
+        {
+            Tile t = c.gameObject.GetComponent<Tile>();
+            if (t != null)
+            {
+                if (!t.isWalkable) isWalkable = false;
+            }
+            else
+            {
+                isWalkable = false;
+            }
+        }
+        return isWalkable;
+    }
+}
