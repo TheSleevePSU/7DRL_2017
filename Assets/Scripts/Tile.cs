8b77c5b41be0713330578bb7a55be380cb4282ef
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-03-05 18:12:24 -0800

    Added basic wall and floor tiles
    Including updates to tag manager

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    public bool isWalkable;
+    public bool blocksProjectiles;
+    public float movementCost = 1;
+}
