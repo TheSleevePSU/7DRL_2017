628c224604c7e620be0d96449acf73b73f6cfb6e
Brett Gildersleeve <brett.gildersleeve@gmail.com>
2017-03-06 23:13:44 -0800

    Enemy can pathfind to closest player
    Path is saved as a list of nodes. Not used to move the enemy yet.

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -11,6 +11,8 @@ public class Enemy : Entity {
 
     public Sword sword;
 
+    private List<Node> pathToClosestPlayer;
+
     // Use this for initialization
     public override void Start()
     {
@@ -21,6 +23,9 @@ public class Enemy : Entity {
     public override void Update()
     {
         ScanForPlayers(this.position, this.sightRange);
+
+        //Debug. TODO: Only call when needed.
+        pathToClosestPlayer = PathfindToPlayer(FindClosestPlayer());
     }
 
     public override void HandleInstruction(Instruction instruction)
@@ -38,4 +43,29 @@ public class Enemy : Entity {
         // TODO: Scan (sightRange) for any instances of Players.
     }
 
+    private Player FindClosestPlayer()
+    {
+        // This first attempt at implementation is lazy and poor performance.
+        // Find all players in scene (slow) and calculate distance to each (slow), then select the closest as the target.
+        // TODO: Figure out how to handle null case, where no players exist in the scene
+        Player closestPlayer = null;
+        float distanceToClosestPlayer = float.MaxValue;
+        Player[] playersInScene = FindObjectsOfType<Player>();
+        foreach (Player p in playersInScene)
+        {
+            float distance = Vector2.Distance(transform.position, p.transform.position);
+            if (distance < distanceToClosestPlayer)
+            {
+                distanceToClosestPlayer = distance;
+                closestPlayer = p;
+            }
+        }
+        return closestPlayer;
+    }
+
+    List<Node> PathfindToPlayer(Player player)
+    {
+        Grid.instance.pathfinding.FindPath(transform.position, player.transform.position);
+        return Grid.instance.path;
+    }
 }
